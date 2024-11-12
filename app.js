2e1f462cf289b51d45c171e55b9ff41662bd806e
SencerBilim <sencer.bilim@gmail.com>
2024-11-12 04:17:14 +0300

    error fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const searchWeather = () => {
             city.querySelector("figcaption").innerText = data.name;
             city.querySelector("img").src= "https://flagsapi.com/"+data.sys.country+"/shiny/32.png"
 
-            temperature.querySelector("img").src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@4x.png"
+            temperature.querySelector("img").src = "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@4x.png"
             temperature.querySelector("figcaption span").innerText = Math.floor(data.main.temp)
             description.innerText = data.weather[0].description;
             clouds.innerText = data.clouds.all;
